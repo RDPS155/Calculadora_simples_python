4567137ac5007e361fa3cc317844ec593a553cf8
RDPS155 <renandps3434@gmail.com>
2025-09-07 08:52:41 -0300

    Add files via upload

---
+++ calculadora.sh
@@ -0,0 +1,18 @@
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite outro número: "))
+formula = input ("Digite a formula: ")
+if formula == '+':
+    print(n1+n2)
+elif formula == '-':
+    print (n1-n2)
+
+elif formula == '*':
+    print (n1*n2)
+
+elif formula == '/':
+    print (n1/n2)
+
+else:
+    print ("Erro")
+
+
